9af90eca19158876cf4591b1c36efe806bfe55a7
idan.liberman <idanlib@gmail.com>
2024-09-28 21:52:16 +0300

    functionality components defined

--- main_Test.ts
+++ main_Test.ts
@@ -1,3 +1,4 @@
+import test from "node:test";
 import {
 	App,
 	Editor,
@@ -35,13 +36,57 @@ export default class RedirectPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		// Adds the referring note count
+		// this.app.workspace.onLayoutReady(() => {
+		// 	const statusBarItemEl = this.addStatusBarItem();
+		// 	// 	setIcon(statusBarItemEl, "corner-up-left");
+
 		this.app.workspace.on("active-leaf-change", (leaf) => {
-			console.log(leaf);
-			// this.test();
+			// 		statusBarItemEl.setText(this.addRedirectCount());
+			this.test();
+		});
+
+		// 	this.app.workspace.on("editor-change", () => {
+		// 		setIcon(statusBarItemEl, "corner-up-left");
+
+		// 		statusBarItemEl.setText(this.addRedirectCount());
+		// 	});
+		// });
+
+		// // This adds a simple command that can be triggered anywhere
+		// this.addCommand({
+		// 	id: "open-sample-modal-simple",
+		// 	name: "Open sample modal (simple)",
+		// 	callback: () => {
+		// 		new RedirectModal(this.app).open();
+		// 	},
+		// 	hotkeys: [{ key: "m", modifiers: ["Alt", "Ctrl", "Meta"] }],
+		// });
+
+		// This adds an editor command that can perform some operation on the current editor instance
+		this.addCommand({
+			id: "paste-your-mama",
+			name: "Pastes 'your mama!' on a selection.",
+
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				editor.replaceSelection("your mama!");
+			},
+			hotkeys: [{ key: "v", modifiers: ["Ctrl", "Alt", "Meta"] }],
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new RedirectSettingsTab(this.app, this));
+
+		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// // Using this function will automatically remove the event listener when this plugin is disabled.
+		// this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+		// 	console.log("click", evt);
+		// });
+
+		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		// this.registerInterval(
+		// 	window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		// );
 
 		this.app.workspace.onLayoutReady(() => {
 			// this.getTargetFile();
@@ -114,6 +159,36 @@ export default class RedirectPlugin extends Plugin {
 	}
 }
 
+// class File extends FileView {
+// 	constructor(leaf: WorkspaceLeaf) {
+// 		super(leaf);
+// 	}
+
+// 	getViewType(): string {
+// 		return "FileView";
+// 	}
+
+// 	getFile() {
+// 		console.log(this.getDisplayText());
+// 	}
+// }
+
+// class RedirectModal extends Modal {
+// 	constructor(app: App) {
+// 		super(app);
+// 	}
+
+// 	onOpen() {
+// 		const { contentEl } = this;
+// 		contentEl.setText("Define redirect here?");
+// 	}
+
+// 	onClose() {
+// 		const { contentEl } = this;
+// 		contentEl.empty();
+// 	}
+// }
+
 class RedirectSettingsTab extends PluginSettingTab {
 	plugin: RedirectPlugin;
 
@@ -142,5 +217,15 @@ class RedirectSettingsTab extends PluginSettingTab {
 					}
 				});
 			});
+
+		// .addText((text) =>
+		// 	text
+		// 		.setPlaceholder("Enter your secret")
+		// 		.setValue(this.plugin.settings.mySetting)
+		// 		.onChange(async (value) => {
+		// 			this.plugin.settings.mySetting = value;
+		// 			await this.plugin.saveSettings();
+		// 		})
+		// );
 	}
 }
