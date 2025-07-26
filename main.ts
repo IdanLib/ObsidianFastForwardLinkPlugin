b1bc22c320e1761127ef59f1d5bbb67f376008e6
idanlib <idanlib@gmail.com>
2025-07-27 00:51:16 +0300

    code review 2 - refactor redirect()

--- main.ts
+++ main.ts
@@ -17,77 +17,33 @@ export default class RedirectPlugin extends Plugin {
 	isMovingNote = false;
 	isBypassRedirect = false;
 
-	handleFileOpen = async (file: TFile) => {
-		if (this.isMovingNote) return;
-
-		if (this.isBypassRedirect) {
-			this.isBypassRedirect = false;
-			return;
-		}
-
-		await this.redirect();
-	};
-	async changeSettings(newTab: boolean, switchToTab: boolean): Promise<void> {
-		this.settings.openInNewTab = newTab;
-		this.settings.switchToNewTab = switchToTab;
-		await this.saveSettings();
-	}
-
-	private async createRedirectsFolder() {
+	private async createRedirectsFolder(): Promise<void> {
 		try {
 			this.redirectsFolder = await this.app.vault.createFolder(
 				"/_forwards"
 			);
 		} catch (error) {
-			new Notice("Failed to create the `_forwards` folder.");
-			console.error(error);
+			console.warn(
+				"`_forwards` folder not created or already exists.",
+				error
+			);
 		}
 	}
 
-	private async redirect() {
+	private async getCurrentFile(): Promise<{
+		currentFile: TFile;
+		content: string;
+	} | null> {
 		const currentFile = this.app.workspace.getActiveFile();
 
 		if (!currentFile) {
-			return;
-		}
-
-		const currentFileContent = (await this.getCurrentFileContent(
-			currentFile
-		)) as string;
-
-		const targetNoteFile = this.getTargetFile(currentFileContent);
-
-		if (!targetNoteFile) {
-			return;
-		}
-
-		if (currentFile.path !== `_forwards/${currentFile.name}`) {
-			new Notice(
-				`Moving ${currentFile.name} to the _forwards folder.`,
-				2000
-			);
-			await this.moveRedirectNote(currentFile);
-		}
-
-		await this.app.workspace.openLinkText(
-			targetNoteFile.name,
-			targetNoteFile.path,
-			this.settings.openInNewTab,
-			{ active: this.settings.switchToNewTab }
-		);
-	}
-
-	private async getCurrentFileContent(
-		currentFile: TFile
-	): Promise<string | null> {
-		try {
-			return await this.app.vault.read(currentFile);
-		} catch (error) {
-			new Notice(`Cannot read ${currentFile}.`);
-			console.error(`Failed to read ${currentFile} content: `, error);
-
 			return null;
 		}
+
+		return {
+			currentFile: currentFile,
+			content: (await this.getCurrentFileContent(currentFile)) || "",
+		};
 	}
 
 	private getTargetFile(currentFileContent: string): TFile | null {
@@ -116,13 +72,107 @@ export default class RedirectPlugin extends Plugin {
 		return targetNoteFile;
 	}
 
+	private async redirect(): Promise<void> {
+		// 1. Get the current file content
+		const fileAndContent = await this.getCurrentFile();
+		if (!fileAndContent) return;
+
+		const { currentFile, content } = fileAndContent;
+
+		// 2. Checks if this is a redirecting note and get the target file
+		const targetNoteFile = this.getTargetFile(content);
+
+		if (!targetNoteFile) return;
+
+		// 3. If the redirecting file is not in the _forwards folder, move it there
+		// if (currentFile.path !== `_forwards/${currentFile.name}`) {
+		// 	new Notice(
+		// 		`Moving ${currentFile.name} to the _forwards folder.`,
+		// 		2000
+		// 	);
+		// 	await this.moveRedirectNote(currentFile);
+		// }
+
+		await this.moveRedirectNote(currentFile);
+
+		// 4. Open the target note
+		await this.openTargetNote(targetNoteFile);
+		// await this.app.workspace.openLinkText(
+		// 	targetNoteFile.name,
+		// 	targetNoteFile.path,
+		// 	this.settings.openInNewTab,
+		// 	{ active: this.settings.switchToNewTab }
+		// );
+	}
+
+	private async openTargetNote(targetNoteFile: TFile) {
+		await this.app.workspace.openLinkText(
+			targetNoteFile.name,
+			targetNoteFile.path,
+			this.settings.openInNewTab,
+			{ active: this.settings.switchToNewTab }
+		);
+	}
+
+	private async getCurrentFileContent(
+		currentFile: TFile
+	): Promise<string | null> {
+		try {
+			return await this.app.vault.read(currentFile);
+		} catch (error) {
+			new Notice(`Cannot read ${currentFile}.`);
+			console.error(`Failed to read ${currentFile} content: `, error);
+
+			return null;
+		}
+	}
+
+	// private getTargetFile(currentFileContent: string): TFile | null {
+	// 	const linkTextRegex = /::>\[\[(.*?)\]\]/i;
+	// 	const targetNoteName = currentFileContent.match(linkTextRegex)?.at(1);
+
+	// 	if (!targetNoteName) {
+	// 		return null;
+	// 	}
+	// 	const targetNoteFile = this.app.metadataCache.getFirstLinkpathDest(
+	// 		getLinkpath(targetNoteName),
+	// 		""
+	// 	);
+
+	// 	if (!targetNoteFile) {
+	// 		new Notice(
+	// 			"Target note not found. Create one and try again.",
+	// 			3500
+	// 		);
+	// 		console.error(
+	// 			`No target note file found for target: ${targetNoteName}. You can create one manually and try again.`
+	// 		);
+	// 		return null;
+	// 	}
+
+	// 	return targetNoteFile;
+	// }
+
 	private async moveRedirectNote(
 		redirectingNote: TFile | null
-	): Promise<TFile | void> {
-		if (!redirectingNote) {
+	): Promise<void> {
+		if (
+			!redirectingNote ||
+			redirectingNote.path === `_forwards/${redirectingNote.name}`
+		) {
 			return;
 		}
 
+		// if (redirectingNote.path === `_forwards/${redirectingNote.name}`) {
+		// 	return;
+		// }
+
+		new Notice(
+			`Moving ${redirectingNote.name} to the _forwards folder.`,
+			2000
+		);
+
+		// If necessary, create _forwards folder
 		if (!this.redirectsFolder) {
 			await this.createRedirectsFolder();
 		}
@@ -135,14 +185,15 @@ export default class RedirectPlugin extends Plugin {
 			);
 
 			if (this.settings.openInNewTab) {
-				await this.app.workspace.openLinkText(
-					redirectingNoteInFolder.name,
-					redirectingNoteInFolder.path,
-					this.settings.openInNewTab,
-					{
-						active: this.settings.switchToNewTab,
-					}
-				);
+				await this.openTargetNote(redirectingNoteInFolder);
+				// await this.app.workspace.openLinkText(
+				// 	redirectingNoteInFolder.name,
+				// 	redirectingNoteInFolder.path,
+				// 	this.settings.openInNewTab,
+				// 	{
+				// 		active: this.settings.switchToNewTab,
+				// 	}
+				// );
 			}
 
 			// Delay deletion to avoid race condition where the original note is deleted before the new note is fully opened in the UI.
@@ -166,6 +217,23 @@ export default class RedirectPlugin extends Plugin {
 			);
 			console.error(error);
 		}
+	}
+
+	handleFileOpen = async (file: TFile): Promise<void> => {
+		if (this.isMovingNote) return;
+
+		if (this.isBypassRedirect) {
+			this.isBypassRedirect = false;
+			return;
+		}
+
+		await this.redirect();
+	};
+
+	async changeSettings(newTab: boolean, switchToTab: boolean): Promise<void> {
+		this.settings.openInNewTab = newTab;
+		this.settings.switchToNewTab = switchToTab;
+		await this.saveSettings();
 	}
 
 	async onload() {
